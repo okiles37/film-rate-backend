8120fe42865eb8c45eed7da6c5d6b7d855678de7
Okan <okan377328@gmail.com>
2026-01-07 17:18:44 +0300

    supabase connection string updated to ipv4 pooler

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,24 +6,31 @@ import { UsersModule } from './users/users.module';
 import { FilmsModule } from './films/films.module';
 import { GenresModule } from './genres/genres.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { WatchlistModule } from './watchlist/watchlist.module'; // BU SATIRI EKLE
+import { WatchlistModule } from './watchlist/watchlist.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-  url: 'postgresql://postgres:SENIN_SIFREN@db.yvyutniyznxnkypawqdb.supabase.co:5432/postgres',
-  autoLoadEntities: true,
-  synchronize: true, // Tabloları Supabase'de senin için otomatik oluşturacak
-  ssl: {
-    rejectUnauthorized: false // Render'ın güvenli bağlanması için bu şart
-  },
+      // PORTU 6543 YAPTIK VE SSL PARAMETRESİNİ EKLEDİK
+      url: 'postgresql://postgres:1U9Z19G1gR0BmZoT@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // Render için kritik
+      },
+      // Bağlantı kopmalarını önlemek için extra ayar
+      extra: {
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+      }
     }),
     UsersModule,
     FilmsModule,
     GenresModule,
     ReviewsModule,
-    WatchlistModule, // BU SATIRI EKLE
+    WatchlistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
