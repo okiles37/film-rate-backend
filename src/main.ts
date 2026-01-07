3aea3c7266fea236201811e24e4497ab952cdbfb
Okan <okan377328@gmail.com>
2026-01-07 17:10:03 +0300

    kesin port ayari

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  
+  // CORS ayarını en basit ve açık hale getirdik
+  app.enableCors(); 
 
- 
+  // Render için '0.0.0.0' eklemesi hayati önem taşır
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0'); 
   
   console.log(`Uygulama port ${port} üzerinde dış dünyaya açıldı.`);
-  });
-
-  // Render'ın dinamik port atamasını (process.env.PORT) önceliğe aldık
-  // Eğer port atanmamışsa yerelde 3000'den çalışmaya devam eder
-  const port = process.env.PORT || 3000;
-  
-  await app.listen(port);
-  console.log(`Uygulama şu port üzerinden yayında: ${port}`);
 }
 bootstrap();
